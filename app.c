ec1a80112e98bfa05b4664043c185fe3c6d2b499
Meiraba <meirabalang.ml.ml@gmail.com>
2025-11-18 00:12:16 +0530

    first

---
+++ app.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Welcome to the shop mamagement system!\n");
+    return 0;
+}
---
+++ redme.md
@@ -0,0 +1,3 @@
+# Shop Inventory Management System
+
+This is a simple app made in c to manage the inventory of a shop.
